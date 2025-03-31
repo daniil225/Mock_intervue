de2b57e2f5570211ebb990e377b35e3fc8de455d
daniil225 <31102560+daniil225@users.noreply.github.com>
2025-03-31 12:59:09 +0200

    add new mock questions

--- Inherit_virtual.cpp
+++ Inherit_virtual.cpp
@@ -1,32 +1,11 @@
 #include <iostream>
 
-struct Base
-{
-    Base() { print(); }
 
-    void print() { std::cout << "Base" << std::endl; }
-
-    virtual ~Base() { print(); }
-};
-
-struct Derived: Base
-{
-    Derived() { print(); throw "error";}
-    virtual void print() { std::cout << "Derived" << std::endl; }
-    virtual ~Derived() { print(); }
-};
 int main()
 {
-    try
-    {
-        Base* base = new Derived();
-        base->print();
-        delete base;
-    } 
-    catch(...)
-    {
-        std::cout << "error";
-    }
+    int* ptr = nullptr;
 
-    return 0;
+    int a = *ptr;
+
+
 }
