836f9009a153120a9eef6a29ab0d1d14aeb51647
daniil225 <31102560+daniil225@users.noreply.github.com>
2025-04-07 07:58:10 +0700

    add

--- Inherit_virtual.cpp
+++ Inherit_virtual.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
 
+template<typename T>
+const T& min(const T& a, const T& b)
+{
+    std::cout << "const T& min(const T& a, const T& b)" << std::endl;
+    return a < b ? a: b;
+}
+
+double min(double a, double b)
+{
+    std::cout << "double min(double a, double b)" << std::endl;
+    return a < b ? a: b;
+}
+
+template<typename T>
+const T& min(const T& a, const T& b, const T& c)
+{
+    std::cout << "const T& min(const T& a, const T& b, const T& c)" << std::endl;
+    T val = min(min(a,b),c);
+
+    std::cout << &val << "\n";
+    return val;
+}
+
+void p(const double& a)
+{
+    std::cout << &a << "\n";
+}
 
 int main()
 {
-    int* ptr = nullptr;
+    double a = 1.0, b = 2.0, c = 3.0;
+    double m = min(a,b,c);
 
-    int a = *ptr;
 
+    std::cout << "val m = " << m << "\n";
+    std::cout << "ptr m = " << &m << "\n";
+
+    std::cout << "Hello world\n"
+    return 0;
 
 }
